78adb78ea7a8c3caa883564db102e29f029548d8
Johnneisha <jwhite4@trinity.edu>
2023-02-12 21:03:24 -0600

    updated program

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from tkinter import *
 
 def main():
     ##READ PREPARED FILE##
-   file = open('DailyAffirmations.csv', 'r', encoding='utf-8-sig')
+   file = open('DailyAffirmations.txt', 'r', encoding='utf-8-sig')
    lines = file.readlines()
    #print(lines)
    file.close()
    ##SELECT AFFIRMATION BASED ON DAY OF YEAR##
    dayofyear = datetime.datetime.today().timetuple().tm_yday
    dailyAffirmation = (lines[dayofyear])
+   print(dailyAffirmation)
 
 if __name__ == "__main__":
     main()
-    
